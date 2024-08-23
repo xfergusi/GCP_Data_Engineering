1cfbc001601ab9a70d06bbd833a5338e5ffe7162
Ian Ferguson <ian@IanFergusonsAir.gateway>
2024-08-23 14:00:52 +1000

    getting two tables from gcp

---
+++ gcpextract.py
@@ -0,0 +1,60 @@
+import os
+
+from google.cloud.sql.connector import Connector, IPTypes
+import pg8000
+
+import sqlalchemy
+
+
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of Postgres.
+
+    Uses the Cloud SQL Python Connector package.
+    """
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+
+    instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]  # e.g. 'project:region:instance'
+    db_user = os.environ["DB_USER"]  # e.g. 'my-db-user'
+    db_pass = os.environ["DB_PASS"]  # e.g. 'my-db-password'
+    db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
+
+    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+
+    # initialize Cloud SQL Python Connector object
+    connector = Connector()
+
+    def getconn() -> pg8000.dbapi.Connection:
+        conn: pg8000.dbapi.Connection = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type=ip_type,
+        )
+        return conn
+
+    # The Cloud SQL Python Connector can be used with SQLAlchemy
+    # using the 'creator' argument to 'create_engine'
+    pool = sqlalchemy.create_engine(
+        "postgresql+pg8000://",
+        creator=getconn,
+        # ...
+    )
+    return pool
+pool = connect_with_connector()
+# interact with Cloud SQL database using connection pool
+with pool.connect() as db_conn:
+    # query database
+    result1 = db_conn.execute(sqlalchemy.text("SELECT * from activity.sms_record limit 1000")).fetchall()
+    result2 = db_conn.execute(sqlalchemy.text("SELECT * from activity.mobile_data_activity limit 1000")).fetchall()
+    print(result1)
+    print(result2)
+    # result = db_conn.execute(sqlalchemy.text("SELECT * FROM activity.sms_record LIMIT 1000")).fetchall()
+    # Do something with the results
+    # for row in result:
+        # print(row)
